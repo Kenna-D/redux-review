85bbeb069e528714866b609c38780b7e2134fee8
Makenna Garrett <kenna.dunford@gmail.com>
2021-03-24 10:27:08 -0600

    completed review

---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+
+import {createStore} from 'redux';
+import moviesReducer from './movieReducer';
+import {devToolsEnhancer} from 'redux-devtools-extension';
+
+let store = createStore(moviesReducer, devToolsEnhancer());
+
+export default store;
